29719c5fe46cf252e77d40ab5069472d1bdac25b
Diogo Soares <69272006+dioggosoares@users.noreply.github.com>
2022-02-18 10:01:53 -0300

    Deletando o arquivo crawler

--- pages/api/crawler.tsx
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function handler(req, res) {
-    await res.unstable_revalidate('/')
-
-    return res.status(200).json({ 
-      name: 'Hello World 🌎 !!!' 
-    })
-}
